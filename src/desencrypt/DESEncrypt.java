c2fd58c1cb91e6e8794b06307e56c2a2272cc377
Dewa Putra <desumputra@gmail.com>
2016-03-19 20:40:31 +0700

    DES first touch!

--- src/desencrypt/DESEncrypt.java
+++ src/desencrypt/DESEncrypt.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package desencrypt;
 
-/**
- *
- * @author DePut
- */
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
 public class DESEncrypt {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        String plain = new BigInteger(in.nextLine().getBytes()).toString(2);
+        System.out.println(plain + " " + plain.length());
+        System.out.println("64 I : "+ plain.substring(0, 63)+ " "+plain.substring(0, 63).length());
+        System.out.println("64 II : "+ plain.substring(64, plain.length())+ " "+plain.substring(64, plain.length()).length());
     }
     
 }
---
+++ src/desencrypt/iDES.java
@@ -0,0 +1,9 @@
+
+package desencrypt;
+
+import java.io.*;
+import java.util.*;
+
+public interface iDES {
+    
+}

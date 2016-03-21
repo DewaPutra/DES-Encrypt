26a2cedb29f5f72fb04a8ae45bb871cf4f090d0a
aryabawanta <arya.bawanta@yahoo.com>
2016-03-21 10:27:21 +0700

    mau buat yang bagi 8

--- src/desencrypt/DESEncrypt.java
+++ src/desencrypt/DESEncrypt.java
@@ -10,9 +10,25 @@ public class DESEncrypt {
     public static void main(String[] args) {
         iDESImplementation c = new iDESImplementation();
         Scanner in = new Scanner(System.in);
-        String plain = c.toBit(in.nextLine());
-        
-        System.out.println(plain + " " + plain.length());
+        String input = in.nextLine();
+        String[] string8 = new String[]{};
+        int counter8=1;
+        int counterLength=0;
+        int index=0;
+        while (counterLength<input.length()){
+            
+            counterLength++;
+            counter8++;
+            if (counter8==9) {
+                counter8=1;
+                index++;
+            }
+                
+            
+        }
+//        String plain = c.toBit();
+//        
+//        System.out.println(plain + " " + plain.length());
         
     }
 }

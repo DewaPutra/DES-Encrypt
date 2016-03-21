d7e19934e4e8c75f74300a9af50d1a073ba3b96a
aryabawanta <arya.bawanta@yahoo.com>
2016-03-21 18:03:33 +0700

    udh dibagi 8, kurang variable untuk nyimpen bit yang udah diubah

--- src/desencrypt/DESEncrypt.java
+++ src/desencrypt/DESEncrypt.java
@@ -11,7 +11,7 @@ public class DESEncrypt {
         iDESImplementation c = new iDESImplementation();
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        String[] string8 = new String[]{""};
+        String[] string8 = new String[8];
         int counter8=1;
         int counterLength=0;
         int index=0;
@@ -20,22 +20,27 @@ public class DESEncrypt {
             temp+=input.substring(counterLength, counterLength+1);
             counterLength++;
             counter8++;
-            if (counter8==9) {
-//                string8[index] = temp;
-                System.out.println(temp);
+            int tempLength = input.length()-counterLength;
+            if (counter8==9 || (tempLength<8 && counterLength==input.length())) {
+                string8[index] = temp;
+//                String af= temp;
+//                System.out.println(string8[index]);
                 counter8=1;
                 index++;
                 temp="";
             }
         }
         
-//        for (int x=0;x<index-1;x++) {
-//            System.out.println(string8[x]);
-//        }
+        //Kurang variable untuk nyimpen plain yang udah diubah
+        for (int x=0;x<index;x++) {
+            System.out.println(string8[x]);
+            String plain = c.toBit(string8[x]);
+            System.out.println(plain + " " + plain.length());
+        }
         
-//        String plain = c.toBit();
-//        
-//        System.out.println(plain + " " + plain.length());
+        
+        
+        
         
     }
 }

ad474094860bde451617eea23fa9a121203d9412
aryabawanta <arya.bawanta@yahoo.com>
2016-03-21 17:16:58 +0700

    coba bagi 8

--- src/desencrypt/DESEncrypt.java
+++ src/desencrypt/DESEncrypt.java
@@ -11,21 +11,28 @@ public class DESEncrypt {
         iDESImplementation c = new iDESImplementation();
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        String[] string8 = new String[]{};
+        String[] string8 = new String[]{""};
         int counter8=1;
         int counterLength=0;
         int index=0;
+        String temp="";
         while (counterLength<input.length()){
-            
+            temp+=input.substring(counterLength, counterLength+1);
             counterLength++;
             counter8++;
             if (counter8==9) {
+//                string8[index] = temp;
+                System.out.println(temp);
                 counter8=1;
                 index++;
+                temp="";
             }
-                
-            
         }
+        
+//        for (int x=0;x<index-1;x++) {
+//            System.out.println(string8[x]);
+//        }
+        
 //        String plain = c.toBit();
 //        
 //        System.out.println(plain + " " + plain.length());

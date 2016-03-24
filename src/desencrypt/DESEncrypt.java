adba20e105262b5e2e11c392ef99f240e9bb8553
aryabawanta <arya.bawanta@yahoo.com>
2016-03-24 12:25:27 +0700

    sudah bisa chipertext diatas 8 char

--- src/desencrypt/DESEncrypt.java
+++ src/desencrypt/DESEncrypt.java
@@ -12,12 +12,28 @@ public class DESEncrypt {
         iDES c = new iDESImplementation();
         Scanner in = new Scanner(System.in);
         String plain = in.nextLine();
+        String dev[] = c.devn(8, plain);
         String key = in.nextLine();
-        plain = c.addNULL(c.toBit(plain));
+//        plain = c.addNULL(c.toBit(plain));
         key = c.addNULL(c.toBit(key));
-        System.out.println(plain.length()+" "+key.length());
-        String chiper = c.DES(plain, key);
-        String conv = c.convertChiper(chiper);
+        String chipertext = "";
+        for (int i=0;i<dev.length;i++){
+//            int devLength=dev[i].length();
+//            while (devLength<8) {
+//                dev[i]+="0";
+//                devLength++;
+//            }
+            dev[i]=c.addNULL(c.toBit(dev[i]));
+            System.out.println(dev[i].length()+" "+key.length());
+            String chiper = c.DES(dev[i], key);
+            String conv = c.convertChiper(chiper);
+            System.out.println("Convert : "+conv);
+            chipertext+=conv;
+            System.out.println("Chipertext : "+chipertext);
+            
+        }
+//        System.out.println(plain.length()+" "+key.length());
+        
         
     }    
     

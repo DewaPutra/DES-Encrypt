9a18188a39abeba387a0f211543b9e7dd8e459c1
Dewa Putra <desumputra@gmail.com>
2016-03-29 17:05:21 +0700

    readfile done!

--- src/desencrypt/CFB.java
+++ src/desencrypt/CFB.java
@@ -1,11 +1,44 @@
 
 package desencrypt;
 
+import static desencrypt.DESEncrypt.dataset;
 import java.util.*;
 import java.io.*;
 import desencrypt.iDES;
 
 public class CFB {
+    
+    public static int lineCounter(String path) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
+        return lines;
+    }
+    
+    public static String[] dataset(String path) throws IOException{
+        int len = lineCounter(path);
+        String d[] = new String[len];
+        BufferedReader br = null;
+        try {
+            String sCurrentLine;
+            int now=0, idx=0;
+            br = new BufferedReader(new FileReader(path));
+            while ((sCurrentLine = br.readLine()) != null) {
+                d[idx]=sCurrentLine;
+                idx+=1;
+            }
+	} catch (IOException e) {
+            e.printStackTrace();
+	} finally {
+            try {
+		if (br != null)br.close();
+            } catch (IOException ex) {
+		ex.printStackTrace();
+            }
+	}
+        return d;
+    }
     
     public static String[] CFBEncrypt(String IV, String Key, String plaintext){
         iDES c = new iDESImplementation();
@@ -47,32 +80,38 @@ public class CFB {
         return plain;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         iDES c = new iDESImplementation();
-        Scanner in = new Scanner(System.in);
-        String IV = "lkjh1a9g";
-        String key = "asdfghjk";
-        System.out.print("Message: ");
-        String plain = in.nextLine();
+        String path = "E:\\ITS\\KULIAH\\SEMESTER 6\\KIJ\\DESEncrypt\\datasetcoba.txt";
+        String d[] = dataset(path);
+        String IV = "", key = "", plain="";
         String finChip="";
         
-        //Encrypting
-        String cfbResult[] = CFBEncrypt(IV, key, plain);
-        String finChipBit = "";
-        for(int i=0;i<cfbResult.length;i++){
-            finChipBit+=cfbResult[i];
-            finChip += c.convertChiper(cfbResult[i]);
-            String d8[] = c.devn(8, cfbResult[i]);
+        for(int k=0;k<d.length;k+=3){
+            int j=k;
+            IV=d[j];
+            key=d[j+1];
+            plain=d[j+2];
+            key = c.toBit(key);
+            
+            //Encrypting
+            String cfbResult[] = CFBEncrypt(IV, key, plain);
+            String finChipBit = "";
+            finChip="";
+            for(int i=0;i<cfbResult.length;i++){
+                finChipBit+=cfbResult[i];
+                finChip += c.convertChiper(cfbResult[i]);
+                String d8[] = c.devn(8, cfbResult[i]);
+            }
+            System.out.println("Final Chiper: "+finChip +" "+ finChip.length());
+
+            //Decrypting
+            String cfbResultDecrypt[] = CFBDecrypt(IV, key, finChipBit);
+            finChip = "";
+            for(int i=0;i<cfbResultDecrypt.length;i++){
+                finChip += c.convertChiper(cfbResultDecrypt[i]);
+            }
+            System.out.println("Final Plain : "+finChip +" "+ finChip.length());
         }
-        System.out.println("Final Chiper: "+finChip +" "+ finChip.length());
-    
-        //Decrypting
-        String cfbResultDecrypt[] = CFBDecrypt(IV, key, finChipBit);
-        finChip = "";
-        for(int i=0;i<cfbResultDecrypt.length;i++){
-            int j=i+1;
-            finChip += c.convertChiper(cfbResultDecrypt[i]);
-        }
-        System.out.println("Final Plain : "+finChip +" "+ finChip.length());
     }
 }
--- src/desencrypt/DESEncrypt.java
+++ src/desencrypt/DESEncrypt.java
@@ -7,16 +7,25 @@ import desencrypt.iDESImplementation;
 
 public class DESEncrypt {
     
+    public static int lineCounter(String path) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
+        return lines;
+    }
     
-    public static void main(String[] args) throws IOException {
-        
-        iDES c = new iDESImplementation();
+    public static String[] dataset(String path) throws IOException{
+        int len = lineCounter(path);
+        String d[] = new String[len];
         BufferedReader br = null;
         try {
             String sCurrentLine;
-            br = new BufferedReader(new FileReader("E:\\ITS\\KULIAH\\SEMESTER 6\\KIJ\\DESEncrypt\\datasetcoba.txt"));
+            int now=0, idx=0;
+            br = new BufferedReader(new FileReader(path));
             while ((sCurrentLine = br.readLine()) != null) {
-                System.out.println(sCurrentLine);
+                d[idx]=sCurrentLine;
+                idx+=1;
             }
 	} catch (IOException e) {
             e.printStackTrace();
@@ -27,20 +36,35 @@ public class DESEncrypt {
 		ex.printStackTrace();
             }
 	}
+        return d;
+    }
     
-//        String dev[]= c.devn(8, plain);
-//        String conv="";
-//        String chiperfix="", plainfix="";
-//        for (int i=0;i<dev.length;i++){
-//            dev[i]=c.addNULL(c.toBit(dev[i]));
-//            String chiper = c.DESen(dev[i], key);
-//            conv = c.convertChiper(chiper);
-//            chiperfix+=conv;
-//            String decrypt = c.DESde(chiper, key);
-//            conv = c.convertChiper(decrypt);
-//            plainfix+=conv;           
-//        }     
-//        System.out.println("Chiper: "+chiperfix+" Plain: "+plainfix);
+    
+    public static void main(String[] args) throws IOException {
+        
+        iDES c = new iDESImplementation();
+        String path ="E:\\ITS\\KULIAH\\SEMESTER 6\\KIJ\\DESEncrypt\\datasetcoba.txt";
+        String d[] = dataset(path);
+        String iv="", plain="", key="";
+        for(int k=0;k<d.length;k+=3){
+            int j=k;
+            iv=d[j];
+            key=d[j+1];
+            plain=d[j+2];
+            key = c.toBit(key);
+            String dev[]= c.devn(8, plain);
+            String conv="";
+            String chiperfix="", plainfix="";
+            for (int i=0;i<dev.length;i++){
+                dev[i]=c.addNULL(c.toBit(dev[i]));
+                String chiper = c.DESen(dev[i], key);
+                conv = c.convertChiper(chiper);
+                chiperfix+=conv;
+                String decrypt = c.DESde(chiper, key);
+                conv = c.convertChiper(decrypt);
+                plainfix+=conv;           
+            }     
+            System.out.println("Chiper: "+chiperfix+" Plain: "+plainfix);
+        }
     }    
-    
 }
